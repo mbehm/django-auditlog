f0450fc9050ad8191ad4133a58be2bf121d8367f
Markus Behm <markus.behm@liveto.io>
2021-08-02 10:59:22 +0300

    Add view action for data access logging

--- auditlog/mixins.py
+++ auditlog/mixins.py
@@ -45,7 +45,7 @@ class LogEntryAdminMixin(object):
     resource_url.short_description = "Resource"
 
     def msg_short(self, obj):
-        if obj.action == LogEntry.Action.DELETE:
+        if obj.action == LogEntry.Action.DELETE or obj.action == LogEntry.Action.VIEW:
             return ""  # delete
         changes = json.loads(obj.changes)
         s = "" if len(changes) == 1 else "s"
@@ -58,7 +58,7 @@ class LogEntryAdminMixin(object):
     msg_short.short_description = "Changes"
 
     def msg(self, obj):
-        if obj.action == LogEntry.Action.DELETE:
+        if obj.action == LogEntry.Action.DELETE or obj.action == LogEntry.Action.VIEW:
             return ""  # delete
         changes = json.loads(obj.changes)
         msg = "<table><tr><th>#</th><th>Field</th><th>From</th><th>To</th></tr>"
